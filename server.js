0ca6995518505d52abdc3c2f32ccc8fc4dac7e89
Mohian Dipta <mohianmostofa@gmail.com>
2021-05-10 21:46:20 +0600

    create register users, bcrypt

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const app = express()
+
+
+const users = []
+
+
 
 app.use('/public', express.static('public'));
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -13,12 +20,30 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+
+app.post('/login', (req, res) => {
+
+})
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) => {
 
+// register users
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
 })
 
 app.listen(3000)
