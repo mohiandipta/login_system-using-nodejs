cc156687c7af31771497bf7b0a817d1d7139afb2
Mohian Dipta <mohianmostofa@gmail.com>
2021-05-10 17:49:40 +0600

    routes, frontend-design done

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use('/public', express.static('public'));
+
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+
+})
+
+app.listen(3000)
